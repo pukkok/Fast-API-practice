b50d531c4ef30482696c28c945001a4e59f86e1a
Pukkok <krystalgggg@gmail.com>
2024-12-05 23:06:10 +0900

    라우터에 대한 설명 추가

--- app/routers/rest_day_info_router.py
+++ app/routers/rest_day_info_router.py
@@ -5,10 +5,12 @@ from urllib.parse import urlencode
 
 router = APIRouter()
 
+# ? 연과 월을 받아 공휴일 restAPI에서 데이터를 불러온다.
 @router.get('/restday-info')
 async def rest_holiday(solYear: str = Query(default="2024"), solMonth: str = Query(default="")):
-
+    # ? 공휴일정보를 가져오는 경로
     GET_POINT = '/getRestDeInfo'
+    # ? API를 불러오기위한 쿼리파라미터 딕셔너리
     query_params = {
         "_type": "json",
         "solYear": solYear,
@@ -19,14 +21,16 @@ async def rest_holiday(solYear: str = Query(default="2024"), solMonth: str = Que
     url = f'{config["BASE_URL"]}{GET_POINT}?{urlencode(query_params)}&ServiceKey={config["SECRET_KEY"]}'
     # print(f"Request URL: {url}")
     
+    # ? AsyncClient를 이용하여 비동기적으로 처리한다.
+    # ? restAPI에 GET요청을 하여 데이터를 불러온다.
     async with httpx.AsyncClient() as client:
-        response = await client.get(url)
+        res = await client.get(url)
     
-    if response.status_code != 200:
-        return {"error": f"API 호출 실패. 상태 코드: {response.status_code}"}
+    if res.status_code != 200:
+        return {"error": "API 호출 실패", "code": f"{res.status_code}"}
     
     try:
-        data = response.json()
+        data = res.json()
     except ValueError:
         return {"error": "응답 데이터를 JSON으로 변환할 수 없습니다."}
     
