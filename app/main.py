68cacf8e99e2282a3c1d1a43fa664d31894e2b2a
Pukkok <krystalgggg@gmail.com>
2024-12-12 14:43:20 +0900

    on_event를 lifeSpan 방식으로 변경

--- app/main.py
+++ app/main.py
@@ -8,22 +8,35 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from app.lib.get_api_url import get_api_url
 from datetime import datetime
 
-# MongoDB 초기화
+# MongoDB Initialization
 class MongoDB:
-    client = AsyncIOMotorClient(Config.MONGODB_URI)
-    db = client["holiday"]
+    client = None
+    db = None
 
-app = FastAPI()
-mongo = MongoDB()
+# * fastapi의 app.on_event를 더이상 사용하지 않는다.
+# ? lifespan 함수를 만들기 위해 사용
+# todo : asynccontextmanager 데코레이터를 사용해 앱의 생명 주기를 관리
+from contextlib import asynccontextmanager
 
-
-@app.on_event("startup")
-async def startup_db_client():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.state.mongo = MongoDB()
+    print("앱 시작: MongoDB 연결 설정 중...")
     try:
-        await mongo.client.server_info()
-        print("MongoDB 연결 완료")
+        app.state.mongo.client = AsyncIOMotorClient(Config.MONGODB_URI)
+        app.state.mongo.db = app.state.mongo.client["holiday"]
+        print("MongoDB 연결 성공")
     except Exception as e:
-        print(f"DB연결 실패: {e}")
+        print(f"MongoDB 연결 실패: {e}")
+        raise e
+    
+    yield
+    
+    print("앱 종료: MongoDB 연결 해제 중...")
+    app.state.mongo.client.close()
+    print("MongoDB 연결 해제 완료")
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 async def root():
@@ -31,24 +44,26 @@ async def root():
 
 # ? 연간 공휴일를 업데이트 한다.
 @app.get("/holiday-update")
-async def holiday_update(year: Optional[int] = None):
+async def holiday_update(req: Request, year: Optional[int] = None):
     # * default는 현재 연도로 생각한다.
     year = year or datetime.now().year
     api_url = get_api_url(year)
 
+    collection = req.app.state.mongo.db.public_holidays
+
     # todo : 저장할 collection이 있는지 먼저 찾아보기
-    this_year_info = await mongo.db.public_holidays.find_one({"base_year": year})
+    this_year_info = await collection.find_one({"base_year": year})
 
     # todo : 공휴일 API에서 데이터 가져오기
     async with httpx.AsyncClient() as client:
-        response = await client.get(api_url)
-        response.raise_for_status()
-        api_data = response.json()
+        res = await client.get(api_url)
+        res.raise_for_status()
+        api_data = res.json()
 
     data = api_data["response"]["body"]["items"]["item"]
     # * 이미 연도 데이터가 있다면 업데이트 하기
     if this_year_info:
-        update_result = await mongo.db.public_holidays.update_one(
+        update_result = await collection.update_one(
             {"_id": this_year_info["_id"]},
             {"$set": {"item": data}}
         )
@@ -60,7 +75,7 @@ async def holiday_update(year: Optional[int] = None):
             "base_year": year,
             "item": data
         }
-        result = await mongo.db.public_holidays.insert_one(new_holiday)
+        result = await collection.insert_one(new_holiday)
         if result.inserted_id:
             return {"code": 200, "msg": "새로운 데이터 생성"}
         else:
@@ -68,10 +83,12 @@ async def holiday_update(year: Optional[int] = None):
         
 # ? holiday-list를 가져온다.
 @app.get("/holiday-list")
-async def holiday_list(year: Optional[int] = None):
+async def holiday_list(req: Request, year: Optional[int] = None):
     # * default는 현재 연도로 생각한다.
     year = year or datetime.now().year
-    this_year_info = await mongo.db.public_holidays.find_one({"base_year": year})
+    collection = req.app.state.mongo.db.public_holidays
+
+    this_year_info = await collection.find_one({"base_year": year})
     if this_year_info:
         # ! ObjectId를 문자열로 변환, str형태가 아니면 에러 발생
         this_year_info["_id"] = str(this_year_info["_id"])
